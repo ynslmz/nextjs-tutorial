54a581eb8576f9a9fc8f62a2f801ca797ea4c62b
Yunus ÖLMEZ <yns4832@gmail.com>
2024-06-10 15:22:09 +0200

    chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -38,6 +38,8 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching Latest Invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -49,6 +51,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    console.log('Fetched Latest Invoices data...');
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
